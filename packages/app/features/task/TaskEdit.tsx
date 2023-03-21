45283d7c169618b82412a07b484d28c637228b4a
gogy9000 <goropashnyj9000@gmail.com>
2023-03-21 13:54:40 +0500

    create task layout

--- packages/app/features/task/TaskEdit.tsx
+++ packages/app/features/task/TaskEdit.tsx
@@ -1,19 +1,46 @@
-import React, { memo } from 'react'
-import {Text,View} from 'app/design/layout'
+import React, { memo, useEffect } from 'react'
+import { Center, VStack, Text, HStack } from 'app/design/layout'
 import { createParam } from 'solito'
 import { useFindByIdQuery } from 'app/api/services/tasks/endpoints/tasksEndpoints'
+import { Layout } from 'app/layouts/Layout'
+import { useForm } from 'react-hook-form'
+import { Field } from 'app/design/ui/form-elements/field/Field'
+import { setForms } from 'app/utils/typedEntries'
+import { TTaskResponse } from 'app/types/task.types'
 
 const { useParam } = createParam()
 export const TaskEdit: React.FC = memo(() => {
-  const [id]=useParam('id')
-  const {data}=useFindByIdQuery({id:id as string},{skip:!id})
+  const [id] = useParam('id')
+  const { data } = useFindByIdQuery({ id: id as string }, { skip: !id })
+  const { control, setValue } = useForm<TTaskResponse>()
+  useEffect(() => {
+    setForms<TTaskResponse>(data, setValue)
+  }, [data])
 
-  console.log(data)
   return (
-    <View>
-      <Text>
-        TaskEdit
-      </Text>
-    </View>
+    <Layout isHasPadding>
+      <Center>
+        {data ? <VStack w={'sm'} space={'xs'} className={'py-3 px-3 items-center  shadow-black-600 rounded-lg  bg-gray-200'}>
+          <Field<TTaskResponse> className={'font-semibold py-0 text-lg text-gray-500'} variant={'filled'}
+                                control={control} name={'title'} />
+          <Field<TTaskResponse> className={'font-semibold py-1 text-lg text-gray-500'} variant={'filled'}
+                                control={control} name={'location'} />
+          <Field<TTaskResponse> multiline numberOfLines={3} className={'font-semibold py-1 text-lg text-gray-500'}
+                                variant={'filled'}
+                                control={control} name={'description'} />
+          <HStack className={'self-stretch justify-between'}>
+            <Text>
+              Обн.
+              {new Date(data.updatedAt).toLocaleDateString('ru')}
+            </Text>
+            <Text>
+              Созд.
+              {new Date(data.updatedAt).toLocaleDateString('ru')}
+            </Text>
+          </HStack>
+
+        </VStack> : null}
+      </Center>
+    </Layout>
   )
 })
--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,13 +1,16 @@
 import { Dripsy } from './dripsy'
 import { NativeBase } from 'app/provider/NativeBase'
 import { ReduxProvider } from 'app/provider/ReduxProvider'
+import { SafeArea } from 'app/provider/safe-area'
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ReduxProvider>
       <NativeBase>
         <Dripsy>
-          {children}
+          <SafeArea>
+            {children}
+          </SafeArea>
         </Dripsy>
       </NativeBase>
     </ReduxProvider>
